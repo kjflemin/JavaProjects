bf1a5502e2f1fdfcad0c5473226a9254f27dbf51
kjflemin <40310914+kjflemin@users.noreply.github.com>
2018-06-18 22:12:32 -0600

    Adding new Java package and class
    Adding a package with a new class.

---
+++ HelloWorld/src/Basics/BasicsOfJava.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package Basics;
+
+/**
+ * @author kjfle
+ *
+ */
+public class BasicsOfJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("New Hello World");
+		
+		int newInt = 5;
+		String newString = "Keith";
+		
+		System.out.println(newString + ":" + newInt);
+
+	}
+
+}
