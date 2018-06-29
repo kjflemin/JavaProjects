30129dde64e994e262e6bd7f2106f846f80ee6d8
kjflemin <40310914+kjflemin@users.noreply.github.com>
2018-06-29 15:02:50 -0600

    Adding new files

--- HelloWorld/src/Basics/BasicsOfJava.java
+++ HelloWorld/src/Basics/BasicsOfJava.java
@@ -3,8 +3,11 @@
  */
 package Basics;
 
+import java.io.Console;
+import java.util.Scanner;
+
 /**
- * @author kjfle
+ * @author Keith
  *
  */
 public class BasicsOfJava {
@@ -13,13 +16,71 @@ public class BasicsOfJava {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("New Hello World");
-		
-		int newInt = 5;
-		String newString = "Keith";
-		
-		System.out.println(newString + ":" + newInt);
 
+	
+
+	}
+
+	public static void doLoop(int mycount) {
+		int cnt = 0;
+		do {
+			System.out.println(mycount + "\n");
+			System.out.println(cnt + "Greater than 5\n");
+			cnt++;
+		} while (cnt < 10);
+	}
+
+	public static void forLoop(int mycount) {
+		for (int cnt = 1; cnt < mycount; cnt++) {
+			System.out.println("cnt\n");
+		}
+	}
+
+	public static void whileLoop(int mycount) {
+		while (mycount > 5) {
+			System.out.println(mycount + "\n");
+		}
+	}
+
+	public static void ifStatement(int myint) {
+		if (myint > 5) {
+			System.out.println("Greater than 5\n");
+		}
+
+		if (myint == 5) {
+			System.out.println("equal 5\n");
+		} else {
+			System.out.println("less than 5\n");
+		}
+
+	}
+
+	public static void switchStatement(int myswitch) {
+
+		switch (myswitch) {
+		case 1:
+			System.out.println("in switch for 1\n");
+			break;
+		case 2:
+		case 3:
+			break;
+		default:
+			break;
+		}
+
+	}
+
+	public static void scannerForInput() {
+		Scanner scanner = new Scanner(System.in);
+		Console console = System.console();
+
+		var valueA = scanner.nextLine();
+		System.out.println(valueA + "\n");
+
+		valueA = console.readLine();
+		System.out.println(valueA + "\n");
+		
+		scanner.close();
 	}
 
 }
---
+++ HelloWorld/src/Basics/MyMath.java
@@ -0,0 +1,33 @@
+package Basics;
+
+//import java.math.*;
+
+/**
+ * 
+ */
+
+
+/**
+ * @author kjfle
+ *
+ */
+public class MyMath {
+
+	public int Add(int a, int b) {
+		return a+b;
+		
+	}
+	
+	public int Subtract(int a, int b) {
+		return a-b;
+	}
+	
+	public int Multiply(int a, int b) {
+		return a*b;
+	}
+	
+	public int Divide(int numerator, int divisor) {
+		return Math.abs(numerator / divisor);
+		
+	}
+}
